fa13b1be8e72961c24e9239dcaa028cb36c9db3b
Vu Hoang <20161728@student.hust.edu.vn>
2018-08-28 16:03:47 +0700

    new day

--- week2/logistic_regression_newtons_method.py
+++ week2/logistic_regression_newtons_method.py
@@ -3,7 +3,7 @@ import numpy as np
 # data manipulattion
 import pandas as pd 
 # matrix data structure
-from pasty import dmatrices
+from patsy import dmatrices
 # for error logging
 import warnings
 
@@ -32,7 +32,7 @@ tol = 1e-8 # convergence tolerance
 
 lam = None # l2-regularization
 # how long to train for?
-max_iter m= 20 # maximum allowed iterations
+max_iter = 20 # maximum allowed iterations
 
 ## data creation settings
 # Covariance measures how two variables move together.
@@ -47,4 +47,138 @@ beta_x, beta_z, beta_v = -4, .9, 1 # true beta coefficients
 var_x, var_z, var_v = 1, 1, 4 # variances of inputs
 
 ## the model specification you want to fit
-formula = 'y ~ x + z + v + np.epp(x) + I(v**2 + z)'
+formula = 'y ~ x + z + v + np.exp(x) + I(v**2 + z)'
+
+## Step 2 - Generate and organize our data
+# The multivariate normal, multinormal or Gaussian distribution is
+# a generalization of the one-dimensional normal
+# distribution to higher dimentions. Such a distribution is
+# specified by its mean and covariance matrix.
+# so we generate values input - (x, v, z) using normal distributions
+
+# A probability distribution is a function that provides us the probabilities
+# of all possible outcomes of a stochastic process.
+
+# ltes keep x and z closely related (height and weight)
+x, z = np.random.multivariate_normal([0,0], [[var_x,r], [r,var_z]],n).T
+# blood pressure
+v = np.random.normal(0,var_v, n)**3
+
+# create a pandas dataframe (esoly parseable object for manipulation)
+A = pd.DataFrame({'x': x, 'z' : z, 'v' : v})
+# compute the log odds for our 3 independent variables
+# using the sigmoid function
+A['log_odds'] = sigmoid(A[['x', 'z', 'v']].dot([beta_x,beta_z,beta_v]) +
+	sigma+np.random.normal(0, 1, n))
+
+
+# compute the probability sample from binomial distribution
+# A binomial random variable is the number of successes x has in n repeated
+# trials of a binomial experiment.
+# The probability distribution of a binomial random variable is called 
+# a binomial disreibution.
+A['y'] = [np.random.binomial(1,p) for p in A.log_odds]
+
+# create a datafrane that encompasses our input data, model formula, and outputs
+y, X = dmatrices(formula, A, return_type='dataframe')
+
+# print it
+X.head(100)
+
+#like dividing by zero 
+def catch_singularity(f):
+	'''Silences LinAlg Errors and throws a warning instead.'''
+
+	def silencer(*args, **kwargs):
+		try:
+			return f(*args, **kwargs)
+		except np.linalg.LinAlgError:
+			warnings.warn('Algorithm terminated - singular Hessian!')
+			return args[0]
+		return silencer
+@catch_singularity
+def newton_step(curr, X, lam=None):
+	'''One native step of Newton's Method'''
+
+	# how to compute inverse? 
+	# http://www.mathwarehouse.com/algebra/matrix/images/square-matrix/inverse-matrix.gif
+		
+	## compute necessary objects
+	# create probability matrix, minimium 2 dimensions, tranpose (flip it)
+	p = np.array(sigmoid(X.dot(curr[:,0])), ndmin=2).T
+	# create weight matrix from it
+	W = np.diag((p*(1-p))[:,0])
+	# derive the hessian
+	hessian = X.T.dot(W).dot(X)
+	# derive the gradient
+	grad = X.T.dot(y-p)
+
+	## regularization step (avoiding overfitting)
+	if lam:
+		# Return the least-squares solution to a linear matrix equation
+		step, *_ = np.linalg.lstsq(hessian + lam*np.eye(curr.shape[0], grad))
+	else:
+		step, *_ = np.linalg.lstsq(hessian, grad)
+
+	## update our
+	beta = curr + step
+
+	return beta
+
+@catch_singularity
+def alt_newton_step(curr, X, lam=None):
+	'''One naive step of Newton's Method'''
+
+	## compute necessary objects
+	p = np.array(sigmoid(X.dot(curr[:,0])), ndmin=2).T
+	W = np.diag((p*(1-p))[:,0])
+	hessian = X.T.dot(W).dot(X)
+	grad = X.T.dot(y-p)
+
+	## regularization
+	if lam:
+		# Compute the inverse of a matrix.
+		step = np.dot(np.linalg.inv(hessian + lam*np.eye(curr.shape[0])), grad)
+	else:
+		step = np.dot(np.linalg.inv(hessian), grad)
+
+	## update our weights
+	beta = curr + step
+
+	return beta 
+
+def check_coefs_convergence(beta_old, beta_new, tol, iters):
+	'''Checks whether the coefficients have converged in the l-infinity norm.
+	Returns True if they have converged, False otherwise.'''
+	# calculate the change in te coefficients
+	coef_change = np.abs(beta_old - beta_new)
+
+	# if change hasn't reached the threshold and we have more iteratons to go,
+	# keep training
+	return not (np.any(coef_change>tol) & (iters < max_iter))
+
+## initial conditions
+# initital coefficients (weight values), 2 copies, we'll update one
+beta_old, beta = np.ones((len(X.columns),1)), np.zeros((len(X.columns),1))
+
+# num iterations we've done so far
+iter_count = 0
+# have we reached convergence?
+coefs_converged = False
+
+# if we haven't reached convergence... (training step)
+while not coefs_converged:
+
+	# set the old coefficients to our current
+	beta_old = beta
+	# perform a single step of newton's optimization on our data,
+	# set our dated beta values
+	beta = newton_step(beta, X, lam=lam)
+	# increment the number of iterations
+	iter_count += 1
+
+	# check dor convergence between our old and new beta values
+	coefs_converged = check_coefs_convergence(beta_old, beta, tol, iter_count)
+
+print('Iterations : {}'.format(iter_count))
+print('Beta : {}'.format(beta))                    
